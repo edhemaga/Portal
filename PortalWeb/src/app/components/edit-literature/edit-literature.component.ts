54b8840474ae58211d1f72e9f286dc54cf2b6da7
Edhem <edim.hadzic@ssst.edu.ba>
2020-07-23 10:27:02 +0200

    test

---
+++ PortalWeb/src/app/components/edit-literature/edit-literature.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { LiteratureService } from 'src/app/shared/literature.service';
+
+@Component({
+  selector: 'app-edit-literature',
+  templateUrl: './edit-literature.component.html',
+  styleUrls: ['./edit-literature.component.css']
+})
+export class EditLiteratureComponent implements OnInit {
+  editLiteratureForm: FormGroup;
+  recvData: any;
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public LiteratureService: LiteratureService
+  ) {
+    this.recvData = data;
+  }
+  ngOnInit() {
+    this.editLiteratureForm = new FormGroup({
+      Title: new FormControl(this.recvData.lit.Title),
+      Group: new FormControl(this.recvData.lit.Group),
+      Link: new FormControl(this.recvData.lit.Link),
+      Email: new FormControl(this.recvData.lit.Email),
+    }
+    );
+  }
+
+  editLiterature(arg){
+    console.log(arg);
+  }
+}
